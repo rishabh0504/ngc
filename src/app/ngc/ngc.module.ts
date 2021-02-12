e1f713793098c3b42e17d21cc03e4fdfc983c0da
rishabh0504 <rishabh.tiwari@synacor.com>
2021-02-12 18:16:10 +0400

    button with icon css

--- src/app/ngc/ngc.module.ts
+++ src/app/ngc/ngc.module.ts
@@ -2,24 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
 import { IconsComponent } from './components/icons/icons.component';
-import { DisabledDirective } from './directives/disabled.directive';
-import { NoActiveDirective } from './directives/no-active.directive';
+
 
 @NgModule({
   declarations: [
     ButtonComponent,
-    IconsComponent,
-    DisabledDirective,
-    NoActiveDirective,
+    IconsComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     ButtonComponent,
-    IconsComponent,
-    DisabledDirective,
-    NoActiveDirective
+    IconsComponent
   ]
 })
 export class NgcModule { }
